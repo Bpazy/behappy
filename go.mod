da499bcabbde36c82fc81dbc7c69f114d8e68b5c
ziyuan <hanziyuan08@outlook.com>
2021-02-21 01:38:34 +0800

    加密算法

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/Bpazy/really
 
 go 1.15
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-resty/resty/v2 v2.4.0
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/spf13/cobra v1.1.3
+	github.com/ugorji/go v1.2.4 // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/resty.v1 v1.12.0
+)
--- root.go
+++ root.go
@@ -1,8 +1,17 @@
 package really
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
+	"gopkg.in/resty.v1"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http/cookiejar"
 )
 
 var (
@@ -39,5 +48,102 @@ func Execute() error {
 }
 
 func Run() {
+	go func() {
+		client := resty.New()
+		jar, err := cookiejar.New(nil)
+		if err != nil {
+			log.Fatalf("%+v", err)
+		}
+		client.SetCookieJar(jar)
 
+		getLoginPageRes, err := client.R().Get("http://dotamax.com/login/")
+		if err != nil {
+			panic(err)
+		}
+
+		body := getLoginPageRes.String()
+		fmt.Println(body)
+		// dom, err := goquery.NewDocumentFromReader(strings.NewReader(body))
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+
+		fmt.Println(encrypt(""))
+
+		client.R().
+			SetFormData(map[string]string{
+				"csrfmiddlewaretoken": "",
+				"phoneNumCipherb64":   "",
+				"usernameCipherb64":   "",
+				"passwordCipherb64":   "",
+				"account-type":        "2",
+				"src":                 "None",
+			}).
+			Post("http://dotamax.com/accounts/login/")
+	}()
+
+	serve()
+}
+
+func encrypt(content string) string {
+	rsaE := 10001
+	rsaN := "B81E72A33686A201B0AC009D679750990E3D168670DC6F9452C24E5A4C299AC002C6C89C3CB38784AEA95D66B7B3E9CA950EB9EEFB4EF61383EDDB67C35727F9CA87EE3238346C66D042B35812179501F472AD4F3BA19E701256FE0435AB856E5C5BEA24A2387153023CD4CD43CDA7260FCC1E2E49C14102C253F559F9A45D59DF5004A017B1239448A9A001D276CAD12535DEDE89FFBD57D75BBC9B575530DDD1B7FAD46064AD3C640CBD017F58981215B2EE17CBE175C36570C5235902818648577234E70E81133B088164F98E605D0D6E69A6095A32A72511E9AC901727B635CE2E8002A7B0EC8D012606903BCB825E60C7B6619FFCED4401E693F5EC68AB"
+
+	n := new(big.Int)
+	n, ok := n.SetString(rsaN, 16)
+	if !ok {
+		panic("public key should be hexadecimal")
+	}
+
+	encryptedData, err := rsa.EncryptPKCS1v15(rand.Reader, &rsa.PublicKey{
+		N: n,
+		E: rsaE,
+	}, []byte(content))
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(encryptedData)
+}
+
+// serve 开启事件上报监听器
+func serve() {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.POST("/post", func(c *gin.Context) {
+		all, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(all))
+		c.JSON(200, nil)
+	})
+	log.Fatal(r.Run("0.0.0.0:10000"))
+}
+
+type FriendMessage struct {
+	SessionKey   string         `json:"sessionKey"`
+	Target       int            `json:"target"`
+	MessageChain []PlainMessage `json:"messageChain"`
+}
+
+type PlainMessage struct {
+	Type string `json:"type"`
+	Text string `json:"text,omitempty"`
+}
+
+func NewPlainMessage(text string) PlainMessage {
+	return PlainMessage{
+		Type: "Plain",
+		Text: text,
+	}
+}
+
+func NewFriendMessage(target int, text string) *FriendMessage {
+	return &FriendMessage{
+		SessionKey: "",
+		Target:     target,
+		MessageChain: []PlainMessage{
+			NewPlainMessage(text),
+		},
+	}
 }
