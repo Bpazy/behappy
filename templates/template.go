b0c7063251349b040f5b29dd6366f0e7b85399c2
ziyuan <hanziyuan08@outlook.com>
2021-07-06 02:06:00 +0800

    多排消息改为模板#2 30%

--- templates/template.go
+++ templates/template.go
@@ -8,17 +8,32 @@ import (
 )
 
 //go:embed *.tmpl
-var winTemplate embed.FS
+var messageTemplates embed.FS
 
 //GetSingleMessage 获取单排的模板消息
 func GetSingleMessage(data map[string]interface{}) (string, error) {
-	tpl, err := template.ParseFS(winTemplate, "*.tmpl")
+	tpl, err := template.ParseFS(messageTemplates, "*.tmpl")
 	if err != nil {
 		return "", err
 	}
 
 	buf := new(bytes.Buffer)
-	err = tpl.ExecuteTemplate(buf, "message.tmpl", data)
+	err = tpl.ExecuteTemplate(buf, "single.tmpl", data)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+//GetMultiMessage 获取组排的模板消息
+func GetMultiMessage(data map[string]interface{}) (string, error) {
+	tpl, err := template.ParseFS(messageTemplates, "*.tmpl")
+	if err != nil {
+		return "", err
+	}
+
+	buf := new(bytes.Buffer)
+	err = tpl.ExecuteTemplate(buf, "multi.tmpl", data)
 	if err != nil {
 		return "", err
 	}
