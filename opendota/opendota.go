f16ec4f7991338cf7bd6e540f530c55c17234883
ziyuan <hanziyuan08@outlook.com>
2022-02-17 21:28:51 +0800

    fix: empty check

--- opendota/opendota.go
+++ opendota/opendota.go
@@ -9,17 +9,21 @@ import (
 )
 
 func GetMatchPlayers(playerID string) []models.MatchPlayer {
+	var mps []models.MatchPlayer
 	r, err := http.Client.R().Get(fmt.Sprintf("https://api.opendota.com/api/players/%s/recentMatches", playerID))
 	if err != nil {
 		logrus.Printf("从 opendota 获取玩家比赛列表失败: %+v", err)
-		return []models.MatchPlayer{}
+		return mps
 	}
 
-	var mps []models.MatchPlayer
 	body := r.Body()
 	err = json.Unmarshal(body, &mps)
 	if err != nil {
 		logrus.Printf("从 opendota 获取玩家比赛列表失败, 源字符串 %s, 错误: %+v", body, err)
+		return mps
+	}
+	if len(mps) == 0 {
+		return mps
 	}
 	return mps[0:1]
 }
