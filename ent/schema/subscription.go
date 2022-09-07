8d047e0012810fec22eb7c203e206076a5cee52c
Ziyuan Han <hanziyuan08@outlook.com>
2022-09-07 17:59:46 +0800

    feat: Add index for table `subscriptions`

--- ent/schema/subscription.go
+++ ent/schema/subscription.go
@@ -3,6 +3,7 @@ package schema
 import (
 	"entgo.io/ent"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
 	"entgo.io/ent/schema/mixin"
 )
 
@@ -11,18 +12,31 @@ type Subscription struct {
 	ent.Schema
 }
 
+const (
+	fieldGroupId = "group_id"
+	fieldSteamId = "steam_id"
+	fieldAlias   = "alias"
+)
+
 // Fields of the Subscription.
 func (Subscription) Fields() []ent.Field {
 	return []ent.Field{
-		field.Int("group_id"),
-		field.String("steam_id"),
-		field.String("alias"),
+		field.Int(fieldGroupId),
+		field.String(fieldSteamId),
+		field.String(fieldAlias),
 	}
 }
 
 func (Subscription) Mixin() []ent.Mixin {
 	return []ent.Mixin{
 		mixin.Time{},
+	}
+}
+
+func (Subscription) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields(fieldGroupId),
+		index.Fields(fieldSteamId),
 	}
 }
 
